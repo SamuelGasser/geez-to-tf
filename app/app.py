137c81ccd389bf36ded60c0b4e152c86df822e43
Samuel <samuelgasser94@gmail.com>
2024-10-22 17:33:37 +0200

    Add app

---
+++ app/app.py
@@ -0,0 +1,39 @@
+import types
+from tf.advanced.app import App
+
+
+KNOWN_RENDS = {'underline', 'sub', 'h3', 'margin', 'super', 'bold', 'ul', 'center', 'small_caps', 'h5', 'italics', 'large', 'spaced', 'smallcaps', 'h6', 'i', 'italic', 'h1', 'sup', 'spat', 'h2', 'below', 'above', 'h4', 'sc', 'b'}
+
+
+def fmt_layout(app, n, **kwargs):
+    return app._wrapHtml(n)
+
+
+class TfApp(App):
+    def __init__(app, *args, **kwargs):
+        app.fmt_layout = types.MethodType(fmt_layout, app)
+        super().__init__(*args, **kwargs)
+        app.rendFeatures = tuple(
+            (f, f[5:]) for f in app.api.Fall() if f.startswith("rend_")
+        )
+        app.isFeatures = tuple(f for f in app.api.Fall() if f.startswith("is_"))
+
+
+    def _wrapHtml(app, n):
+        rendFeatures = app.rendFeatures
+        isFeatures = app.isFeatures
+        api = app.api
+        F = api.F
+        Fs = api.Fs
+        material = f'{F.str.v(n) or ""}{F.after.v(n) or ""}'
+        rClses = " ".join(
+            f"r_{r}" if r in KNOWN_RENDS else "r_"
+            for (fr, r) in rendFeatures
+            if Fs(fr).v(n)
+        )
+        iClses = " ".join(ic for ic in isFeatures if Fs(ic).v(n))
+        if rClses or iClses:
+            material = f'<span class="{rClses} {iClses}">{material}</span>'
+        return material
+
+
---
+++ docs/about.md
@@ -0,0 +1,34 @@
+
+
+
+
+
+# Corpus Repositories - geez-to-tf
+
+## `sourceFormat`
+
+`TEI`
+
+
+## `version`
+
+`0.9.0`
+
+
+## `teiVersion`
+
+`20241022`
+
+
+## `schema`
+
+``
+
+
+## Conversion
+
+Converted from TEI to TF
+
+## See also
+
+*   [transcription](transcription.md)
